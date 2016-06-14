5f34511dcf3985e9b371e599e1001624481815e0
matt2112 <matt_lewis@live.co.uk>
2016-06-14 15:49:48 +0100

    remove mock stream

--- app/mock-streams.ts
+++
@@ -1,9 +0,0 @@
-/*
-import { Stream } from './stream';
-
-export var STREAMS: Stream[] = [
-    { "img": "haha", "user": "Mr Cool", id: 42, online: true, "summary": "This is Mr Cool's twitch stream" },
-    { "img": "nyaaa", "user": "Mr Sensible", id: 24, online: false, "summary": "This is Mr Sensible's twitch stream" },
-    { "img": "hey", "user": "Mr Annoying", id: 13, online: false, "summary": "This is Mr Annoying's twitch stream" }
-];
-*/
